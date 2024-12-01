c5d819b40d95bf196b9978bad4436fbce5e4725d
samcole8 <sam@samcole.me>
2024-12-01 12:49:25 +0000

    feat: voice activation

--- tts.py
+++ tts.py
@@ -2,12 +2,23 @@ import subprocess
 from RealtimeSTT import AudioToTextRecorder
 
 def process_text(text):
-    try:
-        subprocess.run(['wtype', text], check=True)
-    except Exception as e:
-        print(f"Error typing text: {e}")
+    global recording
+    print(f"Detected: {text}")
+    if text.lower() == "start typing.":
+        recording = True
+        print("START")
+    elif text.lower() == "stop typing.":
+        recording = False
+        print("STOP")
+    elif recording:
+        try:
+            subprocess.run(['wtype', text], check=True)
+        except Exception as e:
+            print(f"Error typing text: {e}")
 
 if __name__ == '__main__':
+    recording = False
     recorder = AudioToTextRecorder()
     while True:
         recorder.text(process_text)
+
